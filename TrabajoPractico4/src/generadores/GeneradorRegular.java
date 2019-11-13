e986001ee52c11f83d370fc779f94d6ceee4a412
Esteban Uranga <euadenez8@gmail.com>
2019-11-13 05:50:15 -0300

    Añado path de salida a los generadores

--- TrabajoPractico4/src/generadores/GeneradorRegular.java
+++ TrabajoPractico4/src/generadores/GeneradorRegular.java
@@ -5,26 +5,26 @@ import java.io.IOException;
 import utilitarios.Archivo;
 import grafos.Grafo;
 
-public class GeneradorRegular extends Generador{
-
-	public GeneradorRegular(int cantidadDeNodos,int grado) {
+public class GeneradorRegular extends Generador {
+	public GeneradorRegular(int cantidadDeNodos, int grado) {
 		super(cantidadDeNodos);
 		this.setGradoMin(grado);
 		this.setGradoMax(grado);
 	}
 
 	@Override
-	public Grafo generar() {
+	public Grafo generar(String path) {
+		int grado = this.getGradoMax();
 		int cantidadDeAristas = 0;
-		for(int i = 1; i < grado; i++) {
-			int j ;
-			for(j = 0; j < getCantidadDeNodos() - i; j++) {
+		for (int i = 1; i < grado; i++) {
+			int j;
+			for (j = 0; j < getCantidadDeNodos() - i; j++) {
 				matriz.setValor(j + i, j, 1);
 				cantidadDeAristas++;
 			}
 			grado -= 1;
 		}
-		
+
 		Archivo archivo = new Archivo("grafo.in");
 		try {
 			archivo.guardarGrafo(grafoResultante);
@@ -32,20 +32,20 @@ public class GeneradorRegular extends Generador{
 			// TODO Auto-generated catch block
 			System.out.println("No se puede abrir el archivo.");
 		}
-		
-			grafoResultante = new Grafo(getCantidadDeNodos(),  cantArista, this.matriz);
-			grafoResultante.setPorcentajeAdyacencia((int)((float)cantArista/(cantidadDeNodos*(cantidadDeNodos-1)/2)*100));
-			grafoResultante.setGradoMaximo(this.getGradoMax());
-			grafoResultante.setGradoMinimo(this.getGradoMin());
-			Archivo archivo = new Archivo("grafo.in");
-			try {
-				archivo.guardarGrafo(grafoResultante);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				System.out.println("No se puede abrir el archivo.");
-			}
-			return grafoResultante;
 
+		grafoResultante = new Grafo(getCantidadDeNodos(), cantArista, this.matriz);
+		grafoResultante.setPorcentajeAdyacencia(
+				(int) ((float) cantArista / (cantidadDeNodos * (cantidadDeNodos - 1) / 2) * 100));
+		grafoResultante.setGradoMaximo(this.getGradoMax());
+		grafoResultante.setGradoMinimo(this.getGradoMin());
+		Archivo file = new Archivo(path);
+		try {
+			file.guardarGrafo(grafoResultante);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("No se puede abrir el archivo.");
 		}
-	}
+		return grafoResultante;
 
+	}
+}
