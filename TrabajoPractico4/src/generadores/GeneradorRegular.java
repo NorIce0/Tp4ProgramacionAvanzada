c281fa3ecac6b40368b119b8717a740e3df1b50d
Mauro <asdf>
2019-11-13 05:29:51 -0300

    Wake me up
    I can't wake up

--- TrabajoPractico4/src/generadores/GeneradorRegular.java
+++ TrabajoPractico4/src/generadores/GeneradorRegular.java
@@ -2,6 +2,7 @@ package generadores;
 
 import java.io.IOException;
 
+import utilitarios.Archivo;
 import grafos.Grafo;
 
 public class GeneradorRegular extends Generador{
@@ -14,14 +15,51 @@ public class GeneradorRegular extends Generador{
 
 	@Override
 	public Grafo generar() throws IOException {
+		if ((gradoMax % 2 != 0 && cantidadDeNodos % 2 != 0) || cantidadDeNodos <= gradoMax) {
+			return null;
+		}
 		
-		for(int i = 0; i < getCantidadDeNodos(); i++) {
-			for(int j = i + 1; i < getCantidadDeNodos(); j++) {
-				
+		int listagrados[] = new int[cantidadDeNodos], grado = gradoMax;
+		int mitad = cantidadDeNodos / 2;
+		
+		if (grado % 2 == 1) {
+			for (int i = 0; i < mitad; i++) {
+				this.matriz.setValor(i, mitad + i, 1);
+				cantArista++;
+				listagrados[i]++;
+				listagrados[mitad + i]++;
+			}
+			grado -= 1;
+		}
+		
+		// circulo
+		if (grado > 1) { 
+			int salteo = 1;
+			while (grado <= this.gradoMax && grado>0) {
+				for (int i = 0; i < cantidadDeNodos; i++) {
+					this.matriz.setValor(i, (i + salteo) % cantidadDeNodos, 1);
+					cantArista++;
+					listagrados[i]++;
+					listagrados[(i+salteo) % cantidadDeNodos]++;	
+				}
+				salteo++;
+				grado-=2; 
 			}
 		}
 		
-		return null;
+			grafoResultante = new Grafo(getCantidadDeNodos(),  cantArista, this.matriz);
+			grafoResultante.setPorcentajeAdyacencia((int)((float)cantArista/(cantidadDeNodos*(cantidadDeNodos-1)/2)*100));
+			grafoResultante.setGradoMaximo(this.getGradoMax());
+			grafoResultante.setGradoMinimo(this.getGradoMin());
+			Archivo archivo = new Archivo("grafo.in");
+			try {
+				archivo.guardarGrafo(grafoResultante);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("No se puede abrir el archivo.");
+			}
+			return grafoResultante;
+
+		}
 	}
 
-}
--- TrabajoPractico4/src/generadores/aplicacion.java
+++ TrabajoPractico4/src/generadores/aplicacion.java
@@ -1,13 +1,17 @@
 package generadores;
 
 
+import java.io.IOException;
+
 import grafos.Grafo;
 
 public class aplicacion {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int n = 5;
-		GeneradorPorcentajeAdyacencia gen1 = new GeneradorPorcentajeAdyacencia(n, 55.9);
+		GeneradorRegular gen1 = new GeneradorRegular(n, 5);
 		Grafo g1 = gen1.generar();
+		if (gen1 == null)
+			return;
 		g1.getMatrizDeAdayacencia().mostrarMatriz();
 	}
 }
