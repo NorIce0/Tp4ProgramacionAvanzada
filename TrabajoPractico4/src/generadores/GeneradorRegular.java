06f299f9b7d35c472e768c7184188d410c1f9aec
Mauro <asdf>
2019-11-17 13:16:10 -0300

    Arreglo de Regulares

--- TrabajoPractico4/src/generadores/GeneradorRegular.java
+++ TrabajoPractico4/src/generadores/GeneradorRegular.java
@@ -14,27 +14,68 @@ public class GeneradorRegular extends Generador {
 
 	@Override
 	public Grafo generar(String path) {
+		
+		/*
+		 * Este codigo solamente conecta nodos en anillo. Y solamente llena el triangulo inferior de la matriz.
+		 
+//		int grado = this.getGradoMax();
+//		int cantidadDeAristas = 0;
+//		for (int i = 1; i <= grado; i++) {// recorre todo nodo y le otorga un grado
+//			int j;
+//			for (j = 0; j < getCantidadDeNodos() - i; j++) { //Conecta todas los nodos uno a lado del otro (menos el ultimo con el primero
+//				matriz.setValor(j + i, j, 1);
+//				cantidadDeAristas++;
+//			}
+////			grado -= 1;
+//		}
+//		int []grados = new int[cantidadDeNodos];
+//		for(int i = 0; i < cantidadDeNodos; i++) {
+//			grados[i] = grado;
+//		}
+ * */
+ 
 		int grado = this.getGradoMax();
-		int cantidadDeAristas = 0;
-		for (int i = 1; i < grado; i++) {
-			int j;
-			for (j = 0; j < getCantidadDeNodos() - i; j++) {
-				matriz.setValor(j + i, j, 1);
-				cantidadDeAristas++;
-			}
-			grado -= 1;
+		int cantnodos = getCantidadDeNodos();
+		if (grado % 2 != 0 && cantnodos % 2 != 0 || cantnodos <= grado) { //Si grado y nodo son impar Ó grado es mayor a nodo, salgo.
+			return null;
 		}
-		int []grados = new int[cantidadDeNodos];
-		for(int i = 0; i < cantidadDeNodos; i++) {
-			grados[i] = grado;
+		
+		int listagrados[] = new int[cantnodos]; //creo lista de grados
+		
+		int mitad = cantnodos / 2; //agarro la mitad de la cantidad de nodos, porque cuando hago la estrella, el numero del nodo mas la mitad de los nodos me da el nodo exacto que tiene de frente.
+		
+		if (grado % 2 == 1) { //si grado es impar
+			for (int i = 0; i < mitad; i++) {//Voy armando la estrella en la matriz. 
+				matriz.setValor(i, mitad + i, 1);
+				cantArista++;
+				listagrados[i]++;
+				listagrados[mitad + i]++;
+			}
+			grado -= 1; //Ahora resto el grado por uno, porque ahora todos los nodos son de grado 1, y entonces necesitan menos aristas.
+		}
+		
+		// circulo
+		if (grado > 1) { // Si el grado es 1 ó 0, ni me molesto porque ya esta todo. Si fuese 0, no dibujo nada. Si fuese 1, ya pinte en la anterior. 
+			int salto = 1; //representa que cantidad de nodos saltare en cada iteracion.
+			while (grado>0) {
+
+				for (int i = 0; i < cantnodos; i++) {
+					matriz.setValor(i, (i+salto)%cantnodos, 1);
+					cantArista++;
+					listagrados[i]++;
+					listagrados[(i+salto)%cantnodos]++;	
+				}
+				salto++;
+				grado-=2; //le resto dos porque a todos los nodos le agregue 2 grados
+			}
 		}
 
 		grafo = new Grafo(getCantidadDeNodos(), cantArista, this.matriz);
 		grafo.setPorcentajeAdyacencia(
-				(int) ((float) cantArista / (cantidadDeNodos * (cantidadDeNodos - 1) / 2) * 100));
+				(int) ((float) (cantArista / (cantidadDeNodos * (cantidadDeNodos - 1) / 2)) * 100));
 		grafo.setGradoMaximo(this.getGradoMax());
 		grafo.setGradoMinimo(this.getGradoMin());
-		grafo.setGrados(grados);
+//		grafo.setGrados(grados);
 		Archivo file = new Archivo(path);
 		try {
 			file.guardarGrafo(grafo);
--- TrabajoPractico4/src/generadores/aplicacion.java
+++ TrabajoPractico4/src/generadores/aplicacion.java
@@ -10,31 +10,30 @@ import utilitarios.Archivo;
 public class aplicacion {
 	public static void main(String[] args) throws IOException {
 		int n = 600;
-		
+//		
 		double porcentaje;
-		
-		porcentaje = 40.0;
-		Analisis analizador1 = new Analisis();
-		analizador1.startAnalisisGrafos(n, porcentaje, 10000);
+//		
+//		porcentaje = 40.0;
+//		Analisis analizador1 = new Analisis();
+//		analizador1.startAnalisisGrafos(n, porcentaje, 10000);
 		
 		
 //		porcentaje = 60.0;
 //		Analisis analizador2 = new Analisis();
 //		analizador2.startAnalisisGrafos(n, porcentaje, 10000);
-//		
+		
 //		porcentaje = 90.0;
 //		Analisis analizador3 = new Analisis();
 //		analizador3.startAnalisisGrafos(n, porcentaje, 10000);
-////		g1.getMatrizDeAdayacencia().mostrarMatriz();
 		
 		
 		///////////////////////////////////////
 		
-//		n = 1000;
-//		
-//		porcentaje = 50.0;
-//		Analisis analizadorReg1 = new Analisis();
-//		analizadorReg1.startAnalisisGrafos(n, porcentaje, 10000);
+		n = 6;
+		
+		porcentaje = 100.0;
+		Analisis analizadorReg1 = new Analisis();
+		analizadorReg1.startAnalisisGrafosReg(n, porcentaje, 10000);
 //		
 //		porcentaje = 75.0;
 //		Analisis analizadorReg2 = new Analisis();
